dcdad6ab2e24e6c4865d009f16a4e1ee13d2da0f
Alessandro Ferrero <alessandro.ferrero@utah.edu>
2016-01-25 12:53:42 -0700

    Finished constructor, Evaluate method.

--- Spreadsheet/Formula/Formula.cs
+++ Spreadsheet/Formula/Formula.cs
@@ -18,6 +18,7 @@ namespace Formulas
     /// </summary>
     public class Formula
     {
+        private List<string> form;
         /// <summary>
         /// Creates a Formula from a string that consists of a standard infix expression composed
         /// from non-negative floating-point numbers (using C#-like syntax for double/int literals), 
@@ -40,6 +41,108 @@ namespace Formulas
         /// </summary>
         public Formula(String formula)
         {
+            // It checks if there is at least one token.
+            if (GetTokens(formula).Count() == 0)
+            {
+                new FormulaFormatException("Nothing to calculate");
+                return;
+            }
+            double result;
+            // It checks if the first token is a valid entry as a first token.
+            if (double.TryParse(GetTokens(formula).First(), out result) == false)
+            {
+                foreach (char c in GetTokens(formula).First())
+                {
+                    if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789(".Contains(c) == false)
+                    {
+                        new FormulaFormatException("Only numbers, variables, or a left parenthesis are allowed as the first character.");
+                        return;
+                    }
+                }
+            }
+            int lpar = 0, rpar = 0;
+            foreach (string s in GetTokens(formula))
+            {
+                // It checks if there are any invalid tokens.
+                if (double.TryParse(s, out result) == false)
+                {
+                    foreach (char c in s)
+                    {
+                        if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-*/()".Contains(c) == false)
+                        {
+                            new FormulaFormatException("Invalid character in the expression.");
+                            return;
+                        }
+
+                    }
+                }
+                // It checks that the valid tokens are in the right order.
+                // Example: 
+                bool operatorlpar = true;
+                bool numberrpar = true;
+                if ("xyzXYZ1234567890+-*/().".Contains(s) == false)
+                {
+                    new FormulaFormatException("Invalid character in the expression.");
+                    return;
+                }
+                if ("xyzXYZ1234567890(".Contains(s))
+                {
+                    if (operatorlpar == false)
+                    {
+                        new FormulaFormatException("Not valid operation");
+                        return;
+                    }
+                    numberrpar = true;
+                    operatorlpar = false;
+                }
+                else if ("+-*/".Contains(s))
+                {
+                    if (numberrpar == false)
+                    {
+                        new FormulaFormatException("Not valid operation");
+                        return;
+                    }
+                    numberrpar = false;
+                    operatorlpar = true;
+                }
+                // It checks that at any point the number of left parenthesis in never greater than the number of right parenthesis.
+                if (s == "(")
+                {
+                    lpar += 1;
+                }
+                if (s == ")")
+                {
+                    rpar += 1;
+                    if (rpar > lpar)
+                    {
+                        new FormulaFormatException("Missing left parethesis.");
+                        return;
+                    }
+                }
+            }
+            // It checks that the last token is a number, variable or clsing parenthesis.
+            if (double.TryParse(GetTokens(formula).Last(), out result) == false)
+            {
+                foreach (char c in GetTokens(formula).Last())
+                {
+                    if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-*/()".Contains(c) == false)
+                    {
+                        new FormulaFormatException("Only numbers, variables, or a right parenthesis are allowed as first character.");
+                        return;
+                    }
+                }
+            }
+            // It checks that, in the formula, the number of left and right parenthesis is equal.
+            if (lpar != rpar)
+            {
+                new FormulaFormatException("Missing parenthesis.");
+                return;
+            }
+            // It creates a List of tokens (strings)
+            foreach (string s in GetTokens(formula))
+            {
+                form.Add(s);
+            }
         }
         /// <summary>
         /// Evaluates this Formula, using the Lookup delegate to determine the values of variables.  (The
@@ -52,6 +155,34 @@ namespace Formulas
         /// </summary>
         public double Evaluate(Lookup lookup)
         {
+            Stack<double> values = new Stack<double>();
+            Stack<string> operators = new Stack<string>();
+
+            foreach(string t in form)
+            {
+                double number;
+                double result;
+                if (double.TryParse(t, out number))
+                {
+                    if ("/".Contains(operators.Peek()))
+                    {
+                        if (number == 0)
+                        {
+                            new UndefinedVariableException("Division by zero");
+                            return 0;
+                        }
+                        number = values.Pop() / number;
+                        values.Push(number);
+                        operators.Pop();
+                    }
+                    if ("*".Contains(operators.Peek()))
+                    {
+                        number = values.Pop() * number;
+                        values.Push(number);
+                        operators.Pop();
+                    }
+                }
+            }
             return 0;
         }
 
@@ -122,6 +253,7 @@ namespace Formulas
         /// </summary>
         public FormulaFormatException(String message) : base(message)
         {
+            Console.WriteLine(message);
         }
     }
 
